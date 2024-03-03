33e7af2402cca899038adf2e9ec1d96d30dac18a
Dag <dag.lundberg@hotmail.com>
2024-03-03 22:50:08 +0100

    Pipeline debugger

---
+++ PipelineDebuggers/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+var x = new NeoFontContentPipelineExtension.NeoFontImporter();
+
+var f = x.Import(@"C:\Users\Osmund\Downloads\neo-master\neo-master\NeoTestApp.Code\Content\default_font_map.csv", null);
+Console.WriteLine(f);
